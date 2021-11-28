241dc38b6db5e0fa882cd17f17a8c4ad82b23f68
lucy <kkadangj@gmail.com>
2021-11-28 15:44:47 +0900

    5. 최솟값 구하기

---
+++ src/section01/js/5.js
@@ -0,0 +1,14 @@
+// 최솟값 구하기
+
+const arr = [1, 2, 3, 4, 8, -1];
+
+function answer(arr) {
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i]
+    }
+    return min;
+}
+
+const min = answer(arr);
+console.log("최솟값",min)
